01408b9bd1bd5bd1c2a9d8624acc9ca27f1b60b7
k-gourav <code2change.gourav@gmail.com>
2024-04-28 23:29:03 +0530

    ReactDOM manipulation

--- main.js
+++
@@ -1,4 +0,0 @@
-// const heading = document.querySelector('#root')
-// const headChild = document.createElement('h1')
-
-// heading.appendChild(headChild).textContent = 'Hello World from JavaScript !'
