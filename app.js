edc3cc00fe3b9c1246727a0913a28494dff8a662
k-gourav <code2change.gourav@gmail.com>
2024-05-01 10:30:52 +0530

    code changes

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import  {createRoot } from "react-dom/client";
 
-const parent = React.createElement(                                       
-    "div", 
-    {id: "parent"},
-    React.createElement
-    ("div", 
-    {id: "child"}, 
-    [React.createElement('h1', {}, "Hello there from h1 Tag!"),
-    React.createElement('h2', {}, "Hello there from h2 Tag!"),
-    React.createElement('h3', {}, "Hello there from h3 Tag!")
-]) 
-);         // this is currently an object
+// const parent = React.createElement(                                       
+//     "div", 
+//     {id: "parent"},
+//     React.createElement
+//     ("div", 
+//     {id: "child"}, 
+//     [React.createElement('h1', {}, "Hello there from h1 Tag!"),
+//     React.createElement('h2', {}, "Hello there from h2 Tag!"),
+//     React.createElement('h3', {}, "Hello there from h3 Tag!")
+// ]) 
+// );         // this is currently an object
 
-const heading = React.createElement('h1', {id: 'heading', className: "textHead"}, "Hello World from React");
-console.log(heading);  // object
+// const heading = React.createElement('h1', {id: 'heading', className: "textHead"}, "Hello World from React");
+// console.log(heading);  // object
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 console.log(root)  // object
 
-root.render(parent);   // render converts heading object into h1 element and append to the root
+   // render converts heading object into h1 element and append to the root
